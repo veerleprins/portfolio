d07044c7f9aef7241b90e7bf281b49c9e149b76e
veerleprins <veerleprins@hotmail.com>
2021-05-13 18:22:45 +0200

    Created button component

---
+++ src/components/atoms/Button.svelte
@@ -0,0 +1,33 @@
+<script>
+  // Props
+  export let text
+  export let url
+</script>
+
+<style lang="scss">
+  div {
+    --background-color: #fcd9b8;
+    --hover-color: #e09145;
+    --text-color: black;
+    width: 200px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    a {
+      box-shadow: 5px 5px 10px black;
+      text-decoration: none;
+      border-radius: 2px;
+      padding: 0.5em 1em;
+      background-color: var(--background-color);
+      color: var(--text-color);
+      transition: 0.3s;
+      &:hover {
+        background-color: var(--hover-color);
+      }
+    }
+  }
+</style>
+
+<div>
+  <a href={url}>{text}</a>
+</div>
