b6d11144acebbe6f344abe2ac4d00b5dd5d587d6
veerleprins <veerleprins@hotmail.com>
2021-05-26 21:58:01 +0200

    Added Label component

---
+++ src/components/atoms/Label.svelte
@@ -0,0 +1,24 @@
+<script>
+  // Props
+  export let labelName
+</script>
+
+<style lang="scss">
+  @import 'src/styles/index.scss';
+  label {
+    display: flex;
+    display: -webkit-box;
+    display: -webkit-flex;
+    flex-direction: column;
+    margin-bottom: 2em;
+  }
+  @include size-s {
+    label {
+      width: 100%;
+    }
+  }
+</style>
+
+<label for={labelName}>
+  <slot />
+</label>
