25bc55a37fcd91d8a90ca6f70621fac80f268af4
veerleprins <veerleprins@hotmail.com>
2021-06-02 11:57:55 +0200

    Created clickable list component

---
+++ src/components/atoms/ListItem.svelte
@@ -0,0 +1,20 @@
+<script>
+  // Props
+  export let clickable = false
+  export let href = ''
+</script>
+
+<style lang="scss">
+</style>
+
+{#if clickable}
+  <li>
+    <a {href}>
+      <slot />
+    </a>
+  </li>
+{:else}
+  <li>
+    <slot />
+  </li>
+{/if}
