237f2c16343c85c06f3ab86c2b7d82d2394e82a8
veerleprins <veerleprins@hotmail.com>
2021-06-02 12:00:45 +0200

    Created intro content component

---
+++ src/components/molecules/IntroContent.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Components
+  import Title from '@/components/atoms/Title.svelte'
+  import Button from '@/components/atoms/Button.svelte'
+</script>
+
+<style>
+  p {
+    margin-bottom: 5rem;
+    font-size: 1.25rem;
+  }
+</style>
+
+<Title>Hi, my name is Veerle</Title>
+<p>A Front-end developer who loves being creative and writing code</p>
+<Button href="#work">View my work</Button>
