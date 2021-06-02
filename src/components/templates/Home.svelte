28cfda3d14fed8c25e636b048b1eb6958d94f5a6
veerleprins <veerleprins@hotmail.com>
2021-06-02 12:06:32 +0200

    Changed paths and structured code

--- src/components/templates/Home.svelte
+++ src/components/templates/Home.svelte
@@ -1,54 +1,18 @@
 <script>
   // Components
-  import Button from '../atoms/Button.svelte'
-  import WorkList from '../molecules/WorkList.svelte'
+  import Intro from '@/components/organisms/Intro.svelte'
+  import Cases from '@/components/organisms/Cases.svelte'
+  import Blogs from '@/components/organisms/Blogs.svelte'
+  import About from '@/components/organisms/About.svelte'
+  import Contact from '@/components/organisms/Contact.svelte'
 </script>
 
 <style lang="scss">
-  #home {
-    padding: 15vh 0;
-    p {
-      font-size: 24px;
-      width: 50%;
-      margin-bottom: 2em;
-    }
-  }
-  #work {
-    padding: 15vh 0;
-  }
+  @import 'src/styles/index.scss';
 </style>
 
-<section id="home">
-  <img src="" alt="" />
-  <div>
-    <h1>Front end Developer</h1>
-    <Button text="View my work" url="#work" />
-  </div>
-</section>
-<section id="work">
-  <div>
-    <h2>My Work</h2>
-    <p>Some text here about the projects...</p>
-  </div>
-  <WorkList />
-</section>
-<section id="blogs" />
-<section id="about">
-  <h2>About me</h2>
-  <article>
-    <h3>Let's get acquainted</h3>
-    <p>
-      Hi there! I will introduce myself. My name is Veerle Prins and I live in
-      Hoofddorp, the Netherlands
-    </p>
-  </article>
-</section>
-<section id="contact">
-  <h2>Contact me</h2>
-  <div>
-    <Button
-      text="Contact me"
-      url="mailto:info@veerleprins.nl?subject=Contact"
-    />
-  </div>
-</section>
+<Intro />
+<About />
+<Cases />
+<Blogs />
+<Contact />
