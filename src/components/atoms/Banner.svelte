12e067d56c6a5b5f768618478225fd26d39f60e5
veerleprins <veerleprins@hotmail.com>
2021-06-02 12:05:22 +0200

    Created banner component

---
+++ src/components/atoms/Banner.svelte
@@ -0,0 +1,20 @@
+<script>
+  // Props
+  export let src
+  export let alt
+</script>
+
+<style lang="scss">
+  div {
+    width: 100%;
+    height: 200px;
+    background-color: hotpink;
+    img {
+      object-fit: cover;
+      height: inherit;
+      width: 100%;
+    }
+  }
+</style>
+
+<div><img {src} {alt} /></div>
