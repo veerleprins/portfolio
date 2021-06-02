9f7f607fdb51180acac411c878e34c530fe51f79
veerleprins <veerleprins@hotmail.com>
2021-06-02 12:15:33 +0200

    Created project template component

---
+++ src/components/templates/Project.svelte
@@ -0,0 +1,33 @@
+<script>
+  // Components
+  import Banner from '../atoms/Banner.svelte'
+
+  // Props
+  export let project
+</script>
+
+<style lang="scss">
+  @import 'src/styles/index.scss';
+
+  div {
+    // position: relative;
+    // left: -5vw;
+    // width: 100vw;
+    // background-color: violet;
+    section {
+      width: 80vw;
+      margin: 0 auto;
+    }
+    @include size-m {
+      /* left: 20vw; */
+      /* left: calc(-calc(100vw - 1000px) / 2); */
+    }
+  }
+</style>
+
+<div>
+  <Banner src={project.image_banner} alt={project.image_banner_alt} />
+  <section>
+    <h1>{project.title}</h1>
+  </section>
+</div>
