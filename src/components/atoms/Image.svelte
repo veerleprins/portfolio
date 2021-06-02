a20899e6061078ab06e200fc42dd99b173632c27
veerleprins <veerleprins@hotmail.com>
2021-06-02 11:55:54 +0200

    Created clickable image component

---
+++ src/components/atoms/Image.svelte
@@ -0,0 +1,18 @@
+<script>
+  // Props
+  export let isClickable = false
+  export let href = ''
+  export let src
+  export let alt
+</script>
+
+<style lang="scss">
+  @import 'src/styles/index.scss';
+  img {
+    width: 100%;
+  }
+</style>
+
+{#if !isClickable}
+  <img {src} {alt} />
+{:else}<a {href}> <img {src} {alt} /> </a>{/if}
